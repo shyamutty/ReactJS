c70397aa0de40fca5632776cd82cbaf4b4ef62a3
ShyamNS <shyamutty@gmail.com>
2023-03-17 22:21:44 +0530

    Update Hooks.js

--- Hooks.js
+++ Hooks.js
@@ -18,3 +18,11 @@ export default function InputComponent() {
   ); 
 } 
 
+/*
+
+Hooks also come with a set of rules, that you need to follow while using them. This applies to all React hooks, including the useState hook that you just learned.
+* You can only call hooks at the top level of your component or your own hooks. 
+* You cannot call hooks inside loops or conditions. 
+* You can only call hooks from React functions, and not regular JavaScript functions. 
+
+*/
