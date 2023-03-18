acdbd741ba18ce1acac24293aabc094a45984d6f
ShyamNS <shyamutty@gmail.com>
2023-03-18 17:38:02 +0530

    Create UseState.js

---
+++ UseState.js
@@ -0,0 +1,13 @@
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
